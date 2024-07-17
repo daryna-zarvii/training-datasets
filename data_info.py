470fe09b20d67f73504fa381d5e6b557bf91e06b
daryna-zarvii <149242368+daryna-zarvii@users.noreply.github.com>
2024-07-17 14:55:28 +0200

    Add files via upload

---
+++ data_info.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df = pd.read_csv("C:\Projects\Python\Coding\Work\Portfolio\\airbnb listings\Data\listings.csv")
+
+print(df)
+print(df.info())
+print(df.describe())
+
+input()
